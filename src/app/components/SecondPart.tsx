752b823c61ab902d5b0da855e318df9326d06920
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-07-05 17:45:36 +0200

    changed secondpart component added a typrwriter function

--- src/app/components/SecondPart.tsx
+++ src/app/components/SecondPart.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import { Typewriter, useTypewriter } from "react-simple-typewriter";
 
 type Props = {};
 const TheCode = "<div> h1 hello world  h1</div>";
@@ -16,6 +17,27 @@ const theTryOut = (input: string) => {
 };
 
 function SecondPart({}: Props) {
+  const [finished, setFinished] = useState(false);
+
+  const handleTyping = () => {
+    setFinished(true);
+  };
+
+  const [text, count] = useTypewriter({
+    words: ["<div> h1 hello world  h1</div>"],
+    loop: 1,
+    deleteSpeed: 50,
+    onLoopDone() {
+      setFinished(true);
+    },
+  });
+
+  const [text2, count2] = useTypewriter({
+    words: ["hello world"],
+    loop: 1,
+    deleteSpeed: 50,
+  });
+
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
 
@@ -26,29 +48,32 @@ function SecondPart({}: Props) {
   };
 
   return (
-    <div className="grid  grid-cols-1 gap-5 p-4 sm:grid-cols-2 md:gap-8">
-      <div className="mockup-code">
-        <pre data-prefix="">
-          <code>{TheCode}</code>
-        </pre>
-      </div>
-      <div className="mockup-code ">
-        <pre data-prefix="">
-          <code>{TheCode2}</code>
-        </pre>
-      </div>
+    <div className="mx-auto mt-8 flex h-screen max-w-screen-2xl flex-col items-center justify-center p-4">
+      <h1>You can Create things just with some Code ! try out yourself</h1>
+      <div className="grid   grid-cols-1 gap-5 p-4 sm:grid-cols-2 md:gap-8">
+        <div className="mockup-code h-[200px] w-[600px] ">
+          <pre data-prefix="">
+            <code>{text}</code>
+          </pre>
+        </div>
+        <div className="mockup-code h-[200px] w-[600px] ">
+          <pre data-prefix="">
+            <code>{finished ? text2 : "....."}</code>
+          </pre>
+        </div>
 
-      <div className="mockup-code">
-        <pre data-prefix="">
-          <code>
-            <input type="text" value={input} onChange={handleInputChange} />
-          </code>
-        </pre>
-      </div>
-      <div className="mockup-code">
-        <pre data-prefix="">
-          <code>{result}</code>
-        </pre>
+        <div className="mockup-code h-[200px] w-[600px]">
+          <pre data-prefix="">
+            <code>
+              <input type="text" value={input} onChange={handleInputChange} />
+            </code>
+          </pre>
+        </div>
+        <div className="mockup-code h-[200px] w-[600px]">
+          <pre data-prefix="">
+            <code>{result}</code>
+          </pre>
+        </div>
       </div>
     </div>
   );
